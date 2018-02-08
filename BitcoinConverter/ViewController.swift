d3ff1e6057269d82a962c800b2a3b3313b3b7edb
Priit Pärl <priitparl@gmail.com>
2018-02-08 13:03:57 +0200

    Initial commit

---
+++ BitcoinConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinConverter
+//
+//  Created by Priit Pärl on 08/02/2018.
+//  Copyright © 2018 Priit Pärl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
